9458402b4228fd60ce1a1d5fbce6a2fadecd8d62
filbi <dummy@abc.com>
2016-08-29 17:47:35 +0200

    add command-t v5.0.3

--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ regex='^./pack/\([[:alpha:]]\|-\)\+/start/\([[:alpha:]]\|-\)\+/doc$'
 for dir in $(find . -type d -name doc | grep "$regex"); do
     vim -u NONE "+helptags $dir | q"
 done
+
+cd $HOME/.vim/pack/command-t/start/command-t/ruby/command-t/ext/command-t
+ruby extconf.rb
+make -j8
