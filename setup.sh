ade15194a1a52dd9dcab7ed5b380d30357ecde72
filbi <dummy@abc.com>
2016-08-29 18:12:11 +0200

    add YouCompleteMe

--- setup.sh
+++ setup.sh
@@ -10,3 +10,15 @@ done
 cd $HOME/.vim/pack/command-t/start/command-t/ruby/command-t/ext/command-t
 ruby extconf.rb
 make -j8
+
+cd $HOME/.vim/pack/YouCompleteMe/start/YouCompleteMe
+
+mkdir ycm_build && cd ycm_build
+cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON -DUSE_PYTHON2=OFF . ../third_party/ycmd/cpp/
+cmake --build . --target ycm_core
+cd .. && rm -r ycm_build
+
+mkdir regex_build && cd regex_build
+cmake -G "Unix Makefiles" . ../third_party/ycmd/third_party/cregex
+cmake --build . --target _regex --config Release
+cd .. && rm -r regex_build
