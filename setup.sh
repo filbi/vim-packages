8967ad98070139f42623c22754d1e56149d43327
filbi <dummy@abc.com>
2023-01-10 23:52:25 +0100

    bump ycm

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive
 
 regex='^./pack/\([[:alpha:]]\|-\)\+/start/\([[:alpha:]]\|-\)\+/doc$'
 for dir in $(find . -type d -name doc | grep "$regex"); do
-    vim -u NONE "+helptags $dir | q"
+	vim -u NONE "+helptags $dir | q"
 done
 
 cd $HOME/.vim/pack/command-t/start/command-t/ruby/command-t/ext/command-t
@@ -19,6 +19,6 @@ cmake --build . --target ycm_core
 cd .. && rm -r ycm_build
 
 mkdir regex_build && cd regex_build
-cmake -G "Unix Makefiles" . ../third_party/ycmd/third_party/cregex
-cmake --build . --target _regex --config Release
-cd .. && rm -r regex_build
+cd ./third_party/ycmd/third_party/mrab-regex
+rm -rf ../regex-build && mkdir -p ../regex-build/3
+python setup.py build --build-base=../regex-build/3 --build-lib=../regex-build
